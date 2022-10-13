ec8f1dfbde9db0c572d0801235d86f67211a1d18
kairulla <kayrulla@bk.ru>
2022-10-13 20:20:36 +0600

    создал 1-ю страницу

---
+++ app/src/main/java/kz/lab3_kre17/Activity2.java
@@ -0,0 +1,14 @@
+package kz.lab3_kre17;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
